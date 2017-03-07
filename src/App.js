5d1c96abebb0cc8519b6cfd648980768fcac318b
catherine <catherine.bacon@gmail.com>
2017-03-07 11:27:28 +0000

    add input to add a stock

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// dummy list of stocks
 var stockList = ['AAPL','TSLA','XOM'];
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      stocks: stockList
+      stocks: stockList,
+      newStock: ''
     } 
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      newStock: e.target.value
+    });
+  }
+
+  addStock(e) {
+    e.preventDefault();
+    var stock = this.state.newStock.trim();
+    if (!stock) {
+      return;
+      //// also need to handle invlaid/nonexistant stock
+    }
+    this.setState({
+      stocks: this.state.stocks.concat([stock]),
+      newStock: ''
+    });
   }
 
   render() {
@@ -22,8 +43,18 @@ class App extends Component {
             return <p>{stock}</p>;
           })}
         </p>
+        <form onSubmit={this.addStock.bind(this)}>
+          <input 
+            type='text'
+            placeholder='Stock Code'
+            value={this.state.newStock}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <input type='submit'
+            value='Add' />
+        </form>
       </div>
-    );
+    )
   }
 }
 
