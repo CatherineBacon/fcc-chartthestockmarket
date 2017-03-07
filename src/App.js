768fe6045e2221f3004d10820c2519fe8e373cb3
catherine <catherine.bacon@gmail.com>
2017-03-07 10:28:59 +0000

    update favicon and create list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var stockList = ['AAPL','TSLA','XOM'];
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      stocks: stockList
+    } 
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Catherine</h2>
+          <h2>Chart the Stock Market</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.stocks.map(function(stock) {
+            return <p>{stock}</p>;
+          })}
         </p>
       </div>
     );
